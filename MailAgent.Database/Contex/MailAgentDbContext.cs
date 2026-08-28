307f04a35e1070ac1c46216dd295981ac0da88b9
mta <m.tamazlicaru1959@gmail.com>
2026-08-29 02:31:52 +0300

    Adaugarea claselor de lucru pentru inserare in baza de date

---
+++ MailAgent.Database/Contex/MailAgentDbContext.cs
@@ -0,0 +1,42 @@
+﻿using MailAgent.Database.DataBaseModel;
+using MailAgent.Model.EmailMessage;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailAgent.DataBaseAccess.Contex
+{
+    public class MailAgentDbContext : DbContext
+    {
+
+        public DbSet<EmailMessage> EmailMessages => Set<EmailMessage>();
+
+        public MailAgentDbContext(DbContextOptions<MailAgentDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            ConfigureEmailMessages(modelBuilder);
+
+
+        }
+
+
+        private static void ConfigureEmailMessages(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmailMessage>().ToTable("email_message", "dbo");
+
+            modelBuilder.Entity<EmailMessage>().HasKey(m => m.Id);
+            modelBuilder.Entity<EmailMessage>().Property(m => m.Id).ValueGeneratedNever();
+
+            modelBuilder.Entity<EmailMessage>().Property(m => m.From).HasMaxLength(100);
+            modelBuilder.Entity<EmailMessage>().Property(e => e.Subject).HasMaxLength(500);
+
+        }
+    }
+}
---
+++ MailAgent.Database/DataBaseModel/EmailMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailAgent.Database.DataBaseModel
+{
+    public class EmailMessage
+    {
+        public Guid Id { get; set; }
+
+        public string From { get; set; } = string.Empty;
+
+        public string Subject { get; set; } = string.Empty;
+    }
+}
