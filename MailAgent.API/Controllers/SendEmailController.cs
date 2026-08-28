73bda0ad8f3e451b026f6b09944ae5d7e64fa013
mta <m.tamazlicaru1959@gmail.com>
2026-08-28 17:04:52 +0300

    Add project files.

---
+++ MailAgent.API/Controllers/SendEmailController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailAgent.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendEmailController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult SendEmail()
+        {
+            return Ok("Email sent successfully!");
+        }
+    }
+}
