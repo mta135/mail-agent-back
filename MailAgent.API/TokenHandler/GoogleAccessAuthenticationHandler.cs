bd5d2c8ee0fa476a2a4f0a6e964d87ed081f1554
mta <m.tamazlicaru1959@gmail.com>
2026-09-07 21:47:24 +0300

    Eliminare clasa care nu va fi folosita

--- MailAgent.API/TokenHandler/GoogleAccessAuthenticationHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Authentication;
-
-namespace MailAgent.API.TokenHandler
-{
-    //public class GoogleAccessAuthenticationHandler:AuthenticationHandler<AuthenticationSchemeOptions>
-    //{
-    //}
-}
